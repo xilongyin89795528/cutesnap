35d934915a0a2c2429d1e9e82547977d136d3dc7
XILONG YIN <xilongy@umich.edu>
2025-04-22 22:50:29 -0400

    Update StickerSelector.js

--- src/components/StickerSelector.js
+++ src/components/StickerSelector.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './StickerSelector.css';
 
+/**
+ * StickerSelector Component
+ * 
+ * The component displays a list of stickers and allows the user to add them
+ * When a sticker is clicked, the onAddSticker callback will be triggered, passing the sticker's URL
+ *
+ * @param {Object[]} stickerList - The list of stickers
+ * @param {string} stickerList.id - ID for each sticker
+ * @param {string} stickerList.src - URL of the sticker's image
+ * @param {Function} onAddSticker - A callback function triggered when a sticker is clicked
+ * 
+ * @returns {JSX.Element} The sticker selector componen
+ */
 const StickerSelector = ({ stickerList, onAddSticker }) => {
   return (
     <div className="sticker-selector-wrapper">
