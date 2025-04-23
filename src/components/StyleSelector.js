3ae1d4b7d9c8f3bd8b9d73a1fbdd743bd34d4b38
XILONG YIN <xilongy@umich.edu>
2025-04-22 22:50:38 -0400

    Update StyleSelector.js

--- src/components/StyleSelector.js
+++ src/components/StyleSelector.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './StyleSelector.css';
 
-
-
+/**
+ * StyleSelector Component
+ * 
+ * This component displays a list of style categories and allows the user to select one.
+ * When a category is selected, the onCategoryChange callback will be triggered, updating the selected category.
+ *
+ * @param {Object[]} categoryList - The list of categories
+ * @param {string} categoryList.id - ID for each category
+ * @param {string} categoryList.image - Image of the category icon
+ * @param {string} categoryList.label - Label for the category
+ * @param {string} selectedCategory - ID of the selected category
+ * @param {Function} onCategoryChange - A callback function triggered when a category is selected
+ * 
+ * @returns {JSX.Element} The style selector component.
+ */
 const StyleSelector = ({ categoryList, selectedCategory, onCategoryChange }) => {
     return (
       <div className="style-selector">
